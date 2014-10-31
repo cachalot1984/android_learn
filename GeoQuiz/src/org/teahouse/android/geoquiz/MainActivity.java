566c7e6b4a6a0995cd1097e2d5699c03d3e30986
jtao <jtao@lenotea.(none)>
2014-10-31 22:27:00 +0800

    activity life cycle, logging, persistency

--- GeoQuiz/src/org/teahouse/android/geoquiz/MainActivity.java
+++ GeoQuiz/src/org/teahouse/android/geoquiz/MainActivity.java
@@ -2,6 +2,8 @@ package org.teahouse.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
+	private static final String TAG = "QuizActivity";
+	private static final String QIDX = "qidx";
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private ImageButton mNextButton;
@@ -49,7 +53,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate() called, savedInstanceState");
         setContentView(R.layout.activity_main);
+        
+        if(savedInstanceState != null) {
+        	mCurrentIndex = savedInstanceState.getInt(QIDX, 0);
+        	Log.d(TAG, "restored mCurrentIndex");
+        }
         
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -105,5 +115,42 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		super.onSaveInstanceState(savedInstanceState);
+		Log.i(TAG, "onSaveInstanceState() called");
+		savedInstanceState.putInt(QIDX, mCurrentIndex);
+	}
 
 }
