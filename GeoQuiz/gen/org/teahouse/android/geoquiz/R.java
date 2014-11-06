a31f3f135aa636f29c0ff59f1275334175df94e5
jtao <cachalot1984@163.com>
2014-11-06 21:39:23 +0800

    cheat activity layout

--- GeoQuiz/gen/org/teahouse/android/geoquiz/R.java
+++ GeoQuiz/gen/org/teahouse/android/geoquiz/R.java
@@ -1651,9 +1651,10 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f090030;
         public static final int action_mode_bar_stub=0x7f09002f;
         public static final int action_mode_close_button=0x7f090022;
-        public static final int action_settings=0x7f090046;
+        public static final int action_settings=0x7f090048;
         public static final int activity_chooser_view_content=0x7f090023;
         public static final int always=0x7f090014;
+        public static final int answerTextView=0x7f090041;
         public static final int beginning=0x7f09001b;
         public static final int bottom=0x7f09001e;
         public static final int checkbox=0x7f09002b;
@@ -1667,7 +1668,7 @@ containing a value of this type.
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090024;
         public static final int expanded_menu=0x7f09002a;
-        public static final int false_button=0x7f090043;
+        public static final int false_button=0x7f090045;
         public static final int home=0x7f090000;
         public static final int homeAsUp=0x7f09000e;
         public static final int icon=0x7f090028;
@@ -1677,13 +1678,13 @@ containing a value of this type.
         public static final int list_item=0x7f090027;
         public static final int middle=0x7f09001d;
         public static final int never=0x7f090017;
-        public static final int next_button=0x7f090045;
+        public static final int next_button=0x7f090047;
         public static final int none=0x7f09000f;
         public static final int normal=0x7f09000b;
-        public static final int prev_button=0x7f090044;
+        public static final int prev_button=0x7f090046;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
-        public static final int question_text_view=0x7f090041;
+        public static final int question_text_view=0x7f090043;
         public static final int radio=0x7f09002d;
         public static final int search_badge=0x7f090037;
         public static final int search_bar=0x7f090036;
@@ -1696,6 +1697,7 @@ containing a value of this type.
         public static final int search_src_text=0x7f09003c;
         public static final int search_voice_btn=0x7f090040;
         public static final int shortcut=0x7f09002c;
+        public static final int showAnswerButton=0x7f090042;
         public static final int showCustom=0x7f090010;
         public static final int showHome=0x7f090011;
         public static final int showTitle=0x7f090012;
@@ -1704,7 +1706,7 @@ containing a value of this type.
         public static final int tabMode=0x7f09000c;
         public static final int title=0x7f090029;
         public static final int top=0x7f09001f;
-        public static final int true_button=0x7f090042;
+        public static final int true_button=0x7f090044;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
         public static final int withText=0x7f090018;
@@ -1759,8 +1761,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030014;
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int activity_cheat=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1824,6 +1827,7 @@ containing a value of this type.
         public static final int correct_toast=0x7f0a0014;
         public static final int false_button=0x7f0a0013;
         public static final int incorrect_toast=0x7f0a0015;
+        public static final int judgment_toast=0x7f0a001f;
         public static final int menu_settings=0x7f0a0010;
         public static final int next_button=0x7f0a0016;
         public static final int prev_button=0x7f0a0017;
@@ -1832,7 +1836,9 @@ containing a value of this type.
         public static final int question_asia=0x7f0a001c;
         public static final int question_mideast=0x7f0a0019;
         public static final int question_oceans=0x7f0a0018;
+        public static final int show_answer_button=0x7f0a001e;
         public static final int true_button=0x7f0a0012;
+        public static final int warning_text=0x7f0a001d;
     }
     public static final class style {
         /** 
